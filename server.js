96653f4a491f2569d37d2120d0e227d91b4972a2
Ram Goel <rgoel766@gmail.com>
2021-08-09 16:34:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.post('/download', (req, res) => {
   } else {
     var lnk = req.body.link.split('/').pop().trim();
     console.log(lnk);
-     res.download(path.join(__dirname+ "../", lnk))
+     res.download(path.join(__dirname+ "/", lnk))
      
     
     
